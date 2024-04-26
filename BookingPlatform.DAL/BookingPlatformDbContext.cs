f078a7cb72cc1dc93ba3b7473ceef5a6fd3a164f
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-04-26 21:24:02 +0300

    Creating a Database Layer, Adding the DbContext Class

---
+++ BookingPlatform.DAL/BookingPlatformDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BookingPlatform.Core.Identity;
+using BookingPlatform.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingPlatform.DAL
+{
+    public class BookingPlatformDbContext(DbContextOptions<BookingPlatformDbContext> options) 
+        : DbContext(options)
+    {
+        public DbSet<Tour> Tours { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookingPlatformDbContext).Assembly);
+        }
+    }
+}
