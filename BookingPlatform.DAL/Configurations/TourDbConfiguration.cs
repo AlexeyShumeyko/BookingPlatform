5d372132400da1ea28eeedd59348ac9ee0c0c26d
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-04-26 21:25:26 +0300

    Creating dbConfigurations

---
+++ BookingPlatform.DAL/Configurations/TourDbConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using BookingPlatform.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingPlatform.DAL.Configurations
+{
+    public class TourDbConfiguration : IEntityTypeConfiguration<Tour>
+    {
+        public void Configure(EntityTypeBuilder<Tour> builder)
+        {
+        }
+    }
+}
---
+++ BookingPlatform.DAL/Configurations/UserDbConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using BookingPlatform.Core.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookingPlatform.DAL.Configurations
+{
+    public class UserDbConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+        }
+    }
+}
