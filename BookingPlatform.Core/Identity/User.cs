359f2b066596820b2f2ad156075bba88bd9e47bc
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-04-26 21:20:16 +0300

    Adding User and Review models

---
+++ BookingPlatform.Core/Identity/User.cs
@@ -0,0 +1,27 @@
+﻿using BookingPlatform.Core.Contract;
+
+namespace BookingPlatform.Core.Identity
+{
+    public class User : Entity<int>
+    {
+        private User(string userName, string passwordHash, string email) 
+        { 
+            UserName = userName;
+            PasswordHash = passwordHash;
+            Email = email;
+        }
+
+        public string UserName { get; private set; }
+
+        public string PasswordHash { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Role { get; set; } = string.Empty;
+
+        public static User Create (string userName, string passwordHash, string email)
+        {
+            return new User (userName, passwordHash, email);
+        }
+    }
+}
---
+++ BookingPlatform.Core/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿using BookingPlatform.Core.Contract;
+using BookingPlatform.Core.Identity;
+
+namespace BookingPlatform.Core.Models
+{
+    public class Review : Entity<int>
+    {
+        public string Comment { get; set; } = string.Empty;
+        public int Rating { get; set; }
+        public required User User { get; set; }
+        public required Tour Tour { get; set; }
+
+    }
+}
