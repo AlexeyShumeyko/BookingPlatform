3bfb5cc327dd351c0ed7de6b314cf7a7d6cdbaa6
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-04-27 14:12:18 +0300

    authorization using roles and policies; user controller

---
+++ BookingPlatform.API/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using BookingPlatform.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingPlatform.API.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(string userName, string Email, string password)
+        {
+            await _userService.Register(userName, Email, password);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string emeil, string password)
+        {
+            var token = await _userService.Login(emeil, password);
+
+            HttpContext.Response.Cookies.Append("test-cookies", token);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.Response.Cookies.Delete("test-cookies");
+
+            return Ok();
+        }
+    }
+}
---
+++ BookingPlatform.Application/Auth/AdminRequirement.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace BookingPlatform.Application.Auth
+{
+    public class AdminRequirement : IAuthorizationRequirement { }
+
+    public class AdminAuthorizationHandler : AuthorizationHandler<AdminRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AdminRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == ClaimTypes.Role && c.Value == "Admin"))
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+}
