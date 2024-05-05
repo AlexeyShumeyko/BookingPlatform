8a617e1d25fa2555066f0338e0d77f40f7f2867d
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-05 17:32:51 +0300

    Creating services for reviews

---
+++ BookingPlatform.Application/Interfaces/IReviewService.cs
@@ -0,0 +1,13 @@
+﻿using BookingPlatform.Core.Models;
+
+namespace BookingPlatform.Application.Interfaces
+{
+    public interface IReviewService
+    {
+        public Task AddReview(string claimUserId, int tourId, int rating, string comment);
+
+        public Task AddReview(string claimUserId, int tourId, int rating);
+
+        public IEnumerable<Review> GetTourReviews(int tourId);
+    }
+}
---
+++ BookingPlatform.Application/Services/ReviewService.cs
@@ -0,0 +1,65 @@
+﻿using BookingPlatform.Application.Interfaces;
+using BookingPlatform.Core.Contract;
+using BookingPlatform.Core.Models;
+
+namespace BookingPlatform.Application.Services
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ReviewService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddReview(string claimUserId, int tourId, int rating, string comment)
+        {
+            var repo = _unitOfWork.GetRepository<Review>();
+
+            var userId = Convert.ToInt32(claimUserId);
+
+            var review = new Review()
+            {
+                UserId = userId,
+                TourId = tourId,
+                Rating = rating,
+                Comment = comment
+            };
+
+            repo.Create(review);
+
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task AddReview(string claimUserId, int tourId, int rating)
+        {
+            var repo = _unitOfWork.GetRepository<Review>();
+
+            var userId = Convert.ToInt32(claimUserId);
+
+            var review = new Review
+            {
+                UserId = userId,
+                TourId = tourId,
+                Rating = rating
+            };
+
+            repo.Create(review);
+
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public IEnumerable<Review> GetTourReviews(int tourId)
+        {
+            var repo = _unitOfWork.GetRepository<Review>();
+
+            var listTourReviews = repo
+                .AsQueryable()
+                .Where(r => r.TourId == tourId)
+                .ToList();
+
+            return listTourReviews;
+        }
+    }
+}
