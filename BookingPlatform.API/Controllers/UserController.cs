88a7f48614b359da0e079bac27156270eed85a5c
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-06 15:51:41 +0300

    adding a user creation request model; refactoring

--- BookingPlatform.API/Controllers/UserController.cs
+++ BookingPlatform.API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using BookingPlatform.Application.Interfaces;
+using BookingPlatform.Core.Request;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookingPlatform.API.Controllers
@@ -15,17 +16,22 @@ namespace BookingPlatform.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register(string userName, string Email, string password)
+        public async Task<IActionResult> Register([FromBody]CreateUserRequest request)
         {
-            await _userService.Register(userName, Email, password);
+            await _userService.Register(request.UserName, request.Email, request.Password);
 
             return Ok();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(string emeil, string password)
+        public async Task<IActionResult> Login(string email, string password)
         {
-            var token = await _userService.Login(emeil, password);
+            if (string.IsNullOrEmpty(email) && string.IsNullOrEmpty(password))
+            {
+                return BadRequest();
+            }
+
+            var token = await _userService.Login(email, password);
 
             HttpContext.Response.Cookies.Append("test-cookies", token);
 
---
+++ BookingPlatform.Core/Request/CreateUserRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingPlatform.Core.Request
+{
+    public record CreateUserRequest(
+
+        [Required]
+        [StringLength(20)]
+        string UserName,
+
+        [Required]
+        [EmailAddress]
+        string Email,
+
+        [Required]
+        string Password
+        );
+}
